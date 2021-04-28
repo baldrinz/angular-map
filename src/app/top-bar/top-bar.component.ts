a19d6fbdce94faffbce7402aba5b44ca550a3a57
Armel MEVELLEC <armel.mevellec@nxp.com>
2021-04-28 22:54:24 +0200

    Cleanup

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,14 +1,8 @@
 import {NgModule} from '@angular/core';
 import { Component } from '@angular/core';
-import {MatMenuModule} from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
   exports: [
-    MatButtonModule,
-    MatMenuModule,
-    MatButtonToggleModule,
   ]
 })
 
